8587cce2320e3f5b1f301ed34033ef584c78bfce
Artyom Kashkan <a.kashkan10@gmail.com>
2019-09-27 23:43:15 +0300

    update

---
+++ AccountSystemDemo/DAL/Repositories/AccountRepository.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DAL.Interface.Interfaces;
+using DAL.Interface.DTO;
+
+namespace DAL.Repositories
+{
+    public class AccountRepository : IRepository
+    {
+        AccountContext db;
+        private string filePath;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AccountRepository(AccountContext db)
+        {
+            filePath = "accounts.dat";
+            this.db = new AccountContext();
+        }
+
+        /// <summary>
+        /// Add account to list
+        /// </summary>
+        /// <param name="account"></param>
+        public void Create(AccountDTO account)
+        {
+            if (account == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            db.Accounts.Add(account);
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Get account from list
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Account</returns>
+        public AccountDTO GetByNumber(int id)
+        {
+            return db.Accounts.Find(id);
+        }
+
+        /// <summary>
+        /// Update account
+        /// </summary>
+        /// <param name="account"></param>
+        public void Update(AccountDTO account)
+        {
+            if (account == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+
+            db.Entry(account).Property(o => o.AccountId).IsModified = true;
+            db.SaveChanges();
+        }
+
+        /// <summary>
+        /// Save list to storage
+        /// </summary>
+        /// <param name="storage"></param>
+        public void SaveToStorage(IStorage<AccountDTO> storage)
+        {
+            storage.SaveToStorage(db.Accounts.ToList(), filePath);
+        }
+
+        /// <summary>
+        /// Load list from storage
+        /// </summary>
+        /// <param name="storage"></param>
+        public void LoadFromStorage(IStorage<AccountDTO> storage)
+        {
+            storage.LoadFromStorage(db.Accounts.ToList(), filePath);
+        }
+
+        /// <summary>
+        /// Get list of all accounts
+        /// </summary>
+        /// <returns>List</returns>
+        public IEnumerable<AccountDTO> GetAllAccounts()
+        {
+            return db.Accounts;
+        }
+    }
+}
